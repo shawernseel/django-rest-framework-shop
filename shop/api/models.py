f90c88de2b27d3cb79489fdde9a64e845e896502
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-12 16:45:14 -0700

    added Order model

--- shop/api/models.py
+++ shop/api/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -18,4 +19,22 @@ class Product(models.Model):
     
     def __str__(self): #shows naming for printed objects
         return self.name
-        
+    
+class Order(models.Model):
+    class StatusChoices(models.TextChoices): #like an enum
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+    
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField( #stores text shows status of order in its lifecycle
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING       
+    )
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    
