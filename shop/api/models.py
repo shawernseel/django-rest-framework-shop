ff5c1cb40dd7258351cd8bf56ca6612659fc4a6b
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-12 17:01:07 -0700

    added many to many between Order and Products

--- shop/api/models.py
+++ shop/api/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+#need to add AUTH_USER_MODEL in settings.py
 class User(AbstractUser): #Abstracts Django's default User model
     pass
 
@@ -35,6 +35,21 @@ class Order(models.Model):
         default=StatusChoices.PENDING       
     )
 
+    products = models.ManyToManyField(Product, through="OrderItem", related_name='orders') 
+    #related_name defines access from the other side ex. prodect.orders.all()
+
     def __str__(self):
         return f"Order {self.order_id} by {self.user.username}"
+
+#this is a through model
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
     
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
