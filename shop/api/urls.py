642bacb76507e8af18cb924ac4d151cbf15a6710
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-16 10:26:02 -0700

    switch from function based views to class based views

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/info/', views.product_list),
-    path('products/<int:pk>/', views.product_detail),
-    path('orders/', views.order_list),
+    path('products/', views.ProductListAPIView.as_view()), #we need to use as_view() for class based views
+    path('products/info/', views.product_info),
+    path('products/<int:product_id>/', views.ProductListAPIView.as_view()),
+    path('orders/', views.OrderListAPIView.as_view()),
 ]
--- shop/api/views.py
+++ shop/api/views.py
@@ -4,29 +4,40 @@ from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSeria
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 #function based views
-@api_view(['GET']) #this decorator limits to GET requests #this makes it so you can view the browsable api
-def product_list(request):
-    products = Product.objects.all() #ORM query
-    serializer = ProductSerializer(products, many=True) #for querysets you need many=True
-    return Response(serializer.data)
+# @api_view(['GET']) #this decorator limits to GET requests #this makes it so you can view the browsable api
+# def product_list(request):
+#     products = Product.objects.all() #ORM query
+#     serializer = ProductSerializer(products, many=True) #for querysets you need many=True
+#     return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0) #this will filter out stocks <= 0
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    products = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(products) #converts to json
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def product_detail(request, pk):
+#     products = get_object_or_404(Product, pk=pk)
+#     serializer = ProductSerializer(products) #converts to json
+#     return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    products = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id' #this sets what url argument we use when looking up value in urls.py. Default is 'fk'.
 
-@api_view(['GET'])
-def order_list(request):
-    #orders = Order.objects.all() #bellow is an optomization for this line
-    orders = Order.objects.prefetch_related(
-        #'items' #this is commented out because the below will include this implicitly
-        'items__product' #uses related name to prefetch
-    ) #.all() #prefetch_related automatically includes .all() so we don't need a .all() here
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def order_list(request):
+#     #orders = Order.objects.all() #bellow is an optomization for this line
+#     orders = Order.objects.prefetch_related(
+#         #'items' #this is commented out because the below will include this implicitly
+#         'items__product' #uses related name to prefetch
+#     ) #.all() #prefetch_related automatically includes .all() so we don't need a .all() here
+#     serializer = OrderSerializer(orders, many=True)
+#     return Response(serializer.data)
+class OrderListAPIView(generics.RetrieveAPIView):
+    products = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
 
 @api_view(['GET'])
 def product_info(request):
