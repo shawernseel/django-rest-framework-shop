703e19a260592921f2428050e70b38448d842b25
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-23 22:22:28 -0700

    added auth for /products POST requests

--- shop/api/views.py
+++ shop/api/views.py
@@ -5,7 +5,7 @@ from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny 
 from rest_framework.views import APIView
 
 #function based views
@@ -24,6 +24,11 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_permissions(self):
+        self.permission_classes = [AllowAny] #allow any to use endpoint
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 # @api_view(['GET'])
 # def product_detail(request, pk):
