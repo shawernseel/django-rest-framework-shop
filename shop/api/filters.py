f34c37c51b8e141e3454045140016a5756041880
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-06-13 16:43:26 -0700

    added comments

--- shop/api/filters.py
+++ shop/api/filters.py
@@ -2,11 +2,12 @@ import django_filters
 from api.models import Product
 from rest_framework import filters
 
+#Pure DRF filtering
 class InstockFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(stock__gt=0)
 
-
+#django-filter filtering
 class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
